8620649ffaa21af99bf51701b7d5d3a3be4ec371
AustinHadden <vitovalletori@gmail.com>
2020-01-07 12:59:39 -0600

    fixing heroku bugs? again

--- users/users-model.js
+++ users/users-model.js
@@ -21,8 +21,8 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user);
-
+  const id = await db('users').insert(user);
+  console.log(id);
   return findById(id);
 }
 
